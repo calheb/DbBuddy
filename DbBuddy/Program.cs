3ad85746bd2df6b1d0e15789a38345e212fe1a0c
Caleb <caleb.blake10@gmail.com>
2023-12-30 16:04:48 -0600

    Initial commit. Dbb menu system and basic functionality.

--- DbBuddy/Program.cs
+++ DbBuddy/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
+using System.Xml;
 using Newtonsoft.Json;
 
 namespace DbBuddy
@@ -15,112 +15,214 @@ namespace DbBuddy
 
         static void DisplayMainMenu()
         {
-            connectionStrings = ReadConnectionStrings();
-            if ((String.IsNullOrEmpty(connectionStrings.CurrentDb)))
+            bool validInput = false;
+            while (!validInput)
             {
-                connectionStrings.CurrentDb = "Not set.";
-            }            
-            ShowHeader();
-            Console.WriteLine("Main Menu");
-            Console.WriteLine("---------");
-            Console.WriteLine("Select an option:");
-            Console.WriteLine("[1] Settings");
-            Console.WriteLine("[2] Remote Db");
-            Console.WriteLine("[3] Local Db");
-            Console.WriteLine("[4] Settings");
+                connectionStrings = ReadConnectionStrings();
+                if (String.IsNullOrEmpty(connectionStrings.CurrentDb))
+                {
+                    connectionStrings.CurrentDb = "Not set.";
+                }
 
-            int userInput = int.Parse(Console.ReadLine() ?? null);
+                ShowHeader();
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("---------");
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("[1] Local Db");
+                Console.WriteLine("[2] Remote Db");
+                Console.WriteLine("[3] Settings");
+                Console.WriteLine("[4] Exit");
 
-            if (userInput == 1)
-            {
-                Console.Clear();
-                DisplaySettingsMenu();
-            }
-            else if (userInput == 2)
-            {
-                Console.Clear();
-                ToggleRemoteDbActive();
-            }
-            else if (userInput == 3)
-            {
-                Console.Clear();
-                ToggleLocalDbActive();
-
-            }
-            else
-            {
-                DisplayMainMenu();
+                if (int.TryParse(Console.ReadLine(), out int userInput))
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            Console.Clear();
+                            ToggleLocalDbActive();
+                            validInput = true;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            ToggleRemoteDbActive();
+                            validInput = true;
+                            break;
+                        case 3:
+                            Console.Clear();
+                            DisplaySettingsMenu();
+                            validInput = true;
+                            break;
+                        case 4:
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            Console.Clear();
+                            continue;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    ShowHeader();
+                    Console.WriteLine("Main Menu");
+                    Console.WriteLine("---------");
+                    Console.WriteLine("Select an option:");
+                    Console.WriteLine("[1] Local Db");
+                    Console.WriteLine("[2] Remote Db");
+                    Console.WriteLine("[3] Settings");
+                    Console.WriteLine("[4] Exit");
+                }
             }
         }
 
         static void DisplaySettingsMenu()
         {
-            ShowHeader();
-            Console.WriteLine("Settings");
-            Console.WriteLine("---------");
-            Console.WriteLine("[1] Set Local Db");
-            Console.WriteLine("[2] Set Remote Db");
-            Console.WriteLine("[3] Main Menu");
+            bool validInput = false;
+            while (!validInput)
+            {
+                ShowHeader();
+                Console.WriteLine($"Current project path: \n\t{connectionStrings.ConfigPath} \n");
+                Console.WriteLine($"Current local Db connection string: \n\t{connectionStrings.LocalDbPath}\n");
+                Console.WriteLine($"Current remote Db connection string: \n\t{connectionStrings.RemoteDbPath}\n");
+                Console.WriteLine();
+                Console.WriteLine("Settings");
+                Console.WriteLine("---------");
+                Console.WriteLine("[1] Set Local Db");
+                Console.WriteLine("[2] Set Remote Db");
+                Console.WriteLine("[3] Set project path");
+                Console.WriteLine("[4] Main Menu");
 
-            int userInput = int.Parse(Console.ReadLine() ?? null);
-
-            if (userInput == 1)
-            {
-                Console.Clear();
-                DisplaySetLocalDbMenu();
-            }
-            else if (userInput == 2)
-            {
-                Console.Clear();
-                DisplaySetRemoteDbMenu();
-            }
-            else if (userInput == 3)
-            {
-                Console.Clear();
-                DisplayMainMenu();
-            }
-            else
-            {
-                DisplaySettingsMenu();
+                if (int.TryParse(Console.ReadLine(), out int userInput))
+                {
+                    switch (userInput)
+                    {
+                        case 1:
+                            Console.Clear();
+                            DisplaySetLocalDbMenu();
+                            validInput = true;
+                            break;
+                        case 2:
+                            Console.Clear();
+                            DisplaySetRemoteDbMenu();
+                            validInput = true;
+                            break;
+                        case 3:
+                            Console.Clear();
+                            DisplaySetProjectPath();
+                            validInput = true;
+                            break;
+                        case 4:
+                            Console.Clear();
+                            DisplayMainMenu();
+                            validInput = true;
+                            break;
+                        default:
+                            Console.Clear();
+                            continue;
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    ShowHeader();
+                    Console.WriteLine($"Current project path: \n\t{connectionStrings.ConfigPath} \n");
+                    Console.WriteLine($"Current local Db connection string: \n\t{connectionStrings.LocalDbPath}\n");
+                    Console.WriteLine($"Current remote Db connection string: \n\t{connectionStrings.RemoteDbPath}\n");
+                    Console.WriteLine();
+                    Console.WriteLine("Settings");
+                    Console.WriteLine("---------");
+                    Console.WriteLine("[1] Set Local Db");
+                    Console.WriteLine("[2] Set Remote Db");
+                    Console.WriteLine("[3] Set project path");
+                    Console.WriteLine("[4] Main Menu");
+                }
             }
         }
+
 
         static void DisplaySetLocalDbMenu()
         {
-            ShowHeader();
-            Console.WriteLine("Enter your local Db connection string:");
-            connectionStrings.LocalDbPath = Console.ReadLine() ?? string.Empty;
-            connectionStrings.CurrentDb = "Local Db";
-            SaveConnectionStrings(connectionStrings);
+            while (true)
+            {
+                ShowHeader();
+                Console.WriteLine($"Current local connection string: \n{connectionStrings.LocalDbPath}\n");
+                Console.WriteLine("Enter your local Db connection string or [x] to return to the settings menu:");
+                var userInput = Console.ReadLine() ?? string.Empty;
+
+                if (userInput == "x")
+                {
+                    Console.Clear();
+                    break; // Exits the loop and goes back to the previous menu
+                }
+                else
+                {
+                    connectionStrings.LocalDbPath = userInput;
+                    connectionStrings.CurrentDb = "local Db";
+                    SaveConnectionStrings(connectionStrings);
+                }
+            }
+
+            DisplaySettingsMenu();
         }
+
 
         static void DisplaySetRemoteDbMenu()
         {
-            ShowHeader();
-            Console.WriteLine("Enter your remote Db connection string:");
-            connectionStrings.RemoteDbPath = Console.ReadLine() ?? string.Empty;
-            connectionStrings.CurrentDb = "remote Db";
-            SaveConnectionStrings(connectionStrings);
+            while (true)
+            {
+                ShowHeader();
+                Console.WriteLine($"Current remote connection string: \n{connectionStrings.RemoteDbPath}\n");
+                Console.WriteLine("Enter your remote Db connection string or [x] to return to the settings menu:");
+                var userInput = Console.ReadLine() ?? string.Empty;
+
+                if (userInput == "x")
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    connectionStrings.RemoteDbPath = userInput;
+                    connectionStrings.CurrentDb = "remote Db";
+                    SaveConnectionStrings(connectionStrings);
+                }
+            }
+
+            DisplaySettingsMenu();
         }
+
 
         static void DisplaySetProjectPath()
         {
-            ShowHeader();
-            Console.WriteLine("Enter the complete path to the web.config file in your project:");
-            string userInput = Console.ReadLine() ?? string.Empty;
-            if (userInput == null)
+            while (true)
             {
-                Console.Clear();
-                DisplaySetProjectPath();
+                ShowHeader();
+                Console.WriteLine($"Current Web.config path: \n{connectionStrings.ConfigPath}\n");
+                Console.WriteLine("Enter the complete path to the web.config file in your project or [x] to return to the settings menu:");
+                string userInput = Console.ReadLine() ?? string.Empty;
+
+                if (userInput == null || userInput == "")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else if (userInput == "x")
+                {
+                    Console.Clear();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    SaveProjPath(userInput);
+                    Console.WriteLine("Project path saved.");
+                    break;
+                }
             }
-            else
-            {
-                Console.Clear();
-                SaveProjPath(userInput);
-                Console.WriteLine("Project path saved.");
-                DisplayMainMenu();
-            }
+
+            DisplaySettingsMenu();
         }
+
 
         static void SaveProjPath(string projPath)
         {
@@ -128,21 +230,26 @@ namespace DbBuddy
             SaveConnectionStrings(connectionStrings);
         }
 
-        static void ToggleRemoteDbActive()
-        {
-            connectionStrings.CurrentDb = "Remote Db";
-            SaveConnectionStrings(connectionStrings);
-        }
-
         static void ToggleLocalDbActive()
         {
             connectionStrings.CurrentDb = "Local Db";
+            ReplaceConnectionStringsBlock(connectionStrings.ConfigPath, CreateConnectionStringBlock(true));
             SaveConnectionStrings(connectionStrings);
         }
+
+        static void ToggleRemoteDbActive()
+        {
+            connectionStrings.CurrentDb = "Remote Db";
+            ReplaceConnectionStringsBlock(connectionStrings.ConfigPath, CreateConnectionStringBlock(false));
+            SaveConnectionStrings(connectionStrings);
+        }
+
+
         public static void SaveConnectionStrings(ConnectionStrings connectionStrings)
         {
             string jsonString = JsonConvert.SerializeObject(connectionStrings);
             File.WriteAllText("connectionStrings.json", jsonString);
+            Console.Clear();
             DisplayMainMenu();
         }
 
@@ -158,6 +265,44 @@ namespace DbBuddy
                 return JsonConvert.DeserializeObject<ConnectionStrings>(jsonString);
             }
         }
+
+        static void ReplaceConnectionStringsBlock(string webConfigPath, string newConnectionStringsBlock)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(webConfigPath);
+
+            // Find the existing connectionStrings node
+            XmlNode oldConnectionStringsNode = doc.SelectSingleNode("//connectionStrings");
+
+            // Create a new element for connectionStrings and parse the new block
+            XmlElement newConnectionStringsNode = doc.CreateElement("connectionStrings");
+            newConnectionStringsNode.InnerXml = newConnectionStringsBlock;
+
+            if (oldConnectionStringsNode != null)
+            {
+                // Replace the old node with the new one
+                doc.DocumentElement.ReplaceChild(newConnectionStringsNode, oldConnectionStringsNode);
+            }
+            else
+            {
+                // If the connectionStrings node doesn't exist, append the new one
+                doc.DocumentElement.AppendChild(newConnectionStringsNode);
+            }
+
+            doc.Save(webConfigPath);
+        }
+
+
+
+        static string CreateConnectionStringBlock(bool isLocal)
+        {
+            string connectionString = isLocal ? connectionStrings.LocalDbPath : connectionStrings.RemoteDbPath;
+
+            return
+                   $"<add name=\"localDbName\" connectionString=\"{connectionString}\" providerName=\"System.Data.SqlClient\" />";
+        }
+
+
 
         static void ShowHeader()
         {
